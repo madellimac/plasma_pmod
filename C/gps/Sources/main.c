656fa658224a8194078bf21bbf43331d57b975a7
Bisuketto <hugues.almorin@outlook.com>
2018-04-08 18:20:18 +0200

    Add more debug to gps main source

--- C/gps/Sources/main.c
+++ C/gps/Sources/main.c
@@ -6,11 +6,22 @@ int main(int argc, char ** argv)
 {
     MemoryWrite(CTRL_SL_RST, 1);
 	unsigned int value =0;
-	int debug = 1;
+	int debug = 2;
 	while (1) {
 		if(debug == 1){
 			value = MemoryRead(UART_PMOD_STATUS); // MSByte drives the 2 RBG Led (6 bit), LSByte drives the led
 			MemoryWrite(CTRL_SL_RW, value); // drive the LEDs with value
+		}
+		else if(debug == 2){
+			char test[3];
+			test[0] = getchar_pmod();
+			test[1] = '\n';
+			test[2] = '\0';
+			if(test[0] != 0)
+				MemoryWrite(CTRL_SL_RW, 0x00000010);
+			else
+				MemoryWrite(CTRL_SL_RW, 0x00000000);
+			puts(test);
 		}
 		else{
 			struct Gps_Data_GPGGA gpsdat;
