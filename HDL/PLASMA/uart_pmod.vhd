f7462275795d703d2174e74df641dda27d72af6b
Bisuketto <hugues.almorin@outlook.com>
2018-04-08 17:41:52 +0200

    Fifo component added to receive uart

--- HDL/PLASMA/uart_pmod.vhd
+++ HDL/PLASMA/uart_pmod.vhd
@@ -46,6 +46,9 @@ architecture logic of uart_pmod is
              dat_en : out STD_LOGIC);
    end component;
    
+   signal dat_recv : STD_LOGIC_VECTOR(7 downto 0);
+   signal dat_recv_en : STD_LOGIC;
+   
    component UART_send 
       Port ( 
              clk    : in  STD_LOGIC;
@@ -56,13 +59,24 @@ architecture logic of uart_pmod is
              busy   : out STD_LOGIC);
    end component;
    
+   component my_fifo
+      port(
+           clk          : in  std_logic;
+           reset        : in  std_logic;
+           read_en      : in  std_logic;
+           write_en     : in  std_logic;
+           data_in      : in  std_logic_vector(7 downto 0);
+           data_out     : out std_logic_vector(7 downto 0);
+           data_avail   : out std_logic);
+   end component;
+   
 begin
 
 unit_recv : UART_recv port map( clk     => clk,
                                 reset   => reset,
                                 rx      => uart_read,
-                                dat     => data_out,
-                                dat_en  => data_avail
+                                dat     => dat_recv,
+                                dat_en  => dat_recv_en
                               );           
                                                   
 unit_send : UART_send port map( clk     => clk,
@@ -72,5 +86,96 @@ unit_send : UART_send port map( clk     => clk,
                                 TX      => uart_write,
                                 busy    => busy_write
                               );
-
+                              
+unit_fifo_recv : my_fifo port map(    clk           => clk,
+                                      reset         => reset,
+                                      read_en       => enable_read,
+                                      write_en      => dat_recv_en,
+                                      data_in       => dat_recv,
+                                      data_out      => data_out,
+                                      data_avail    => data_avail
+                                  );
+  
 end; --architecture logic
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.all;
+
+entity my_fifo is
+   generic (FIFO_LENGTH : integer range 0 to 127 := 8
+   );
+   port(
+        clk          : in  std_logic;
+        reset        : in  std_logic;
+        read_en      : in  std_logic;
+        write_en     : in  std_logic;
+        data_in      : in  std_logic_vector(7 downto 0);
+        data_out     : out std_logic_vector(7 downto 0);
+        data_avail   : out std_logic);
+end; --entity my_fifo
+
+architecture logic of my_fifo is
+   type reg is array (0 to FIFO_LENGTH-1) of std_logic_vector(7 downto 0);
+   
+   signal fifo : reg;
+   signal sempty, sfull, looped : STD_LOGIC;
+   signal head, tail : integer range 0 to FIFO_LENGTH-1;
+
+begin
+
+fifo_operation : process(clk)
+begin
+    if rising_edge(clk) then
+        if reset = '1' then
+            head <= 0;
+            tail <= 0;
+            looped <= '0';
+        else
+            if write_en = '1' then
+                if looped = '0' or head /= tail then
+                    fifo(tail) <= data_in;
+                    if tail = FIFO_LENGTH-1 then
+                        looped <= '1';
+                        tail <= 0;
+                    else
+                        tail <= tail + 1;
+                    end if;
+                end if;
+            end if;
+            
+            if read_en = '1' then
+                if looped = '1' or head /= tail then
+                    if head = FIFO_LENGTH-1 then
+                        looped <= '0';
+                        head <= 0;
+                    else
+                        head <= head + 1;
+                    end if;
+                end if;
+            end if;
+                
+        end if;
+    end if;
+end process fifo_operation;
+
+asynchrone : process(head, tail, looped)
+begin
+    if head = tail then
+        if looped = '1' then
+            sfull <= '1';
+            sempty <= '0';
+        else
+            sfull <= '0';
+            sempty <= '1';
+        end if;
+    else
+        sfull <= '0';
+        sempty <= '0';
+    end if;
+end process asynchrone;
+
+   data_out <= fifo(head);   
+   data_avail <= not sempty;
+   
+end;
